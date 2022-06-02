a9d917a11bd17f7b19b9aa4dc5ed841528fcd0be
woifdog <105352999+woifdog@users.noreply.github.com>
2022-06-02 20:52:57 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,4 @@
-import discord
-import googletrans
-import os
-from pprint import pprint
-# 輸入自己Bot的TOKEN碼
-TOKEN = os.environ['TOKEN']
-SRCLanguage=os.environ['SRC']
-DSTLanguage=os.environ['DST']
 
-client = discord.Client()
-
-# 起動時呼叫
-@client.event
-async def on_ready():
-    print('成功登入')
-
-# 收到訊息時呼叫
-@client.event
 async def on_message(message):
     # 送信者為Bot時無視
     if message.author.bot:
