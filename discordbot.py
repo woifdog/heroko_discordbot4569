ae5a6dcad480d6aaa5d1827c3160fbb9650e4c57
woifdog <105352999+woifdog@users.noreply.github.com>
2022-06-02 20:36:32 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,7 +26,7 @@ async def on_message(message):
     
     if message.content == 'w/luckynumber':
         num = random(1,10)
-        await message.channel.send('你今天的幸運數字是' + num)
+        await message.channel.send('你今天的幸運數字是')
     if client.user in message.mentions: # @判定
         translator = googletrans.Translator()
         robotName = client.user.name
