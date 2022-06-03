036a5738fb3aa4a75836cb5860e469c2c9e0a961
woifdog <105352999+woifdog@users.noreply.github.com>
2022-06-03 10:32:40 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -38,7 +38,7 @@ async def on_message(message):
             remessage = translator.translate(content, dest='en').text
             await message.reply(remessage) 
         
-while not seif.bot.is_closed():
+while not message.bot.is_closed():
     await message.channel.send('機器人在線中...')
     await asyncio.sleep(120)
 # Bot起動
